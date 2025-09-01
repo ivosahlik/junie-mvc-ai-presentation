65fd9750ce3a59433107d5f5d487bcd67f4949be
Ivo Vošahlík, External <ivosahlik@seznam.cz>
2025-09-01 23:19:11 +0200

    first commit

---
+++ src/test/java/cz/ivosahlik/juniemvcaipresentation/JunieMvcAiPresentationApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.ivosahlik.juniemvcaipresentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunieMvcAiPresentationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
